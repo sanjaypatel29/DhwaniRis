792d95c379a4ac7ebdb907eeac10c4c10bccb7cf
Sanjay Patel <worldprotv@gmail.com>
2020-12-18 14:34:40 +0530

    fileName changed

---
+++ Frontend_task_2/script.js
@@ -0,0 +1,24 @@
+function togglebox1() {
+    var box1 = document.getElementById("box1");
+    box1.classList.toggle("zIndex")
+}
+
+function togglebox2() {
+    var box2 = document.getElementById("box2")
+    box2.classList.toggle("zIndex")
+}
+
+function togglebox3() {
+    var box3 = document.getElementById("box3")
+    box3.classList.toggle("zIndex")
+}
+
+function togglebox4() {
+    var box4 = document.getElementById("box4")
+    box4.classList.toggle("zIndex")
+}
+
+function togglebox5() {
+    var box5 = document.getElementById("box5")
+    box5.classList.toggle("zIndex")
+}
